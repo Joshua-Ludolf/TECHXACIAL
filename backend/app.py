1b330c93df15bf698fd794061787aeab047c0162
Joshua-Ludolf <Jludo01@jaguar.tamu.edu>
2024-10-27 02:44:19 -0500

    Added mySQL

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,17 @@
 import g
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
+import mysql.connector
+import sql
+
+config = {
+        'user': 'root',
+        'password': 'jaguarJosh-25',
+        'host': '127.0.0.1',
+        'port': '3306',
+        'database': 'finances'
+    }
+
 
 app = Flask(__name__)
 CORS(app, origins='*') 
@@ -14,11 +25,11 @@ def main():
     return jsonify({'response': response})
 
 
-@app.route("/education", methods=['GET', 'POST'])
-def handle_data():
-    data = request.get_json()
-    response = g.groq(data['input'])
-    return jsonify({'response': response})
+@app.route('/add_money', methods=['GET', 'POST'])
+def add_money():
+    sql.add_money(connection=mysql.connector.connect(**config))
+    return {'response': "Money Added"}
+
 
 if __name__ == "__main__":
     app.run(debug=True)

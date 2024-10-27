a33ea0e9f920feb3a058935c03cdf7fd13435035
jspinerola <144980273+jspinerola@users.noreply.github.com>
2024-10-26 21:08:45 -0500

    added 2 new components: Balance and Buttons

--- oh-snap/src/main.jsx
+++ oh-snap/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Contact from "./routes/Contact.jsx";
 
-createRoot(document.getElementById('root')).render(
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route index={true} path="/" element={<App />} />
+        <Route path="/:id" element={<App />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);

af7f9cda5dd117716af4f04f8aaeb38ada7533bb
Joshua-Ludolf <Jludo01@jaguar.tamu.edu>
2024-10-26 23:09:15 -0500

    Update the PostRequest component to use the new api

--- oh-snap/src/components/PostRequest.jsx
+++ oh-snap/src/components/PostRequest.jsx
@@ -4,29 +4,12 @@ import axios from "axios";
 function PostRequest() {
   const [response, setResponse] = useState(null);
 
-  const api = axios.create({
-    baseURL: "http://locahost:5000",
-  });
 
-  const handlePostRequest = () => {
-    console.log("Making GET request");
-    api
-      .get("/")
-      .then((res) => {
-        console.log(res.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data", error);
-      });
-  };
 
   return (
-    <div>
-      erm
-      <button onClick={handlePostRequest}>Make Post Request</button>
-      {response && <div>Response: {JSON.stringify(response)}</div>}
-      <div>PostRequest</div>
-    </div>
+    <>
+    </>
+
   );
 }
 
